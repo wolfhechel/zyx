d3ce128c96dd315e1b242690541cdb84501f3aff
Pontus Karlsson <pontuscarlsson@live.se>
2016-08-08 18:36:02 +0200

    Ported from zynfw

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='zyx',
+    version='',
+    url='https://github.com/wolfhechel/zyn',
+    license='',
+    author='Pontus Karlsson',
+    author_email='pontus@jensenkarlsson.se',
+    install_requires=[
+        'pyserial<3',
+        'xmodem',
+        'progressbar2',
+        'pyelftools'
+    ],
+    packages=[
+        'zyx'
+    ],
+    description='Zyxel utilities',
+    entry_points={
+        'console_scripts': [
+            'zyx = zyx.__main__:main'
+        ]
+    },
+)
